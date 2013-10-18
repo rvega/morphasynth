787e923c856aa229f312416dfe25382bcfa45a78
luis felipe quiros cardenas <luisfelipequiroscardenas@iMac-de-luis-felipe-quiros-cardenas.local>
2013-10-18 03:08:46 -0500

    Buttons name

--- html/js/Button.js
+++ html/js/Button.js
@@ -15,8 +15,9 @@ Morphasynth.Button = function(){
   var angle = 0;
   var anglOff = 125;
   var ledArray = new Array();
-  var ledoff = "#488089";
+  var ledoff = "#263B43";
   var ledon = "#B9E9E1";
+  var nameText;
 
   this.init = function (nombre, canvas){
 
@@ -39,15 +40,15 @@ Morphasynth.Button = function(){
     posX = px;
     posY = py;
 
-    but = synthCanvas.path("M23.5,13.029 L0.5,26.058 L-22.5,13.029 L-22.5,-13.029 L0.5,-26.058 L23.5,-13.029 Z");
-    butin = synthCanvas.path("M20,11.539 L0,23.078 L-20,11.539 L-20,-11.539 L0,-23.078 L20,-11.539 Z");
-    butLine = synthCanvas.path("M-17.949,12.761 L-2.785,1.838 L-20,9.486 L-20,11.539 Z");
+    but = synthCanvas.path("M16.5,10.498 L0,19.737 L-16.5,10.498 L-16.5,-7.982 L0,-17.222 L16.5,-7.982 Z");
+    butin = synthCanvas.path("M14,9.441 L0,17.624 L-14,9.441 L-14,-6.926 L0,-15.109 L14,-6.926 Z");
+    butLine = synthCanvas.path("M-12.684,10.308 L-1.861,2.561 L-14,7.985 L-14,9.441 Z");
 
     var currAng = 0;
     for(var i = 0; i<15 ; i++){
-      var led = synthCanvas.path("M-22.916,34.707c-0.409-0.249-0.813-0.503-1.216-0.763l4.323-6.683c0.343,0.222,0.688,0.437,1.036,0.649L-22.916,34.707z");
+      var led = synthCanvas.path("M-15.602,24.539c-0.217-0.132-0.432-0.267-0.645-0.404l2.292-3.543c0.182,0.117,0.364,0.231,0.549,0.344L-15.602,24.539z");
       led.attr({'fill':ledoff,'stroke-width':'0'});
-      led.transform("T"+px+", "+py+"r"+((i*20)+5)+", "+0+", "+0);
+      led.transform("T"+px+", "+py+"r"+((i*20)+9)+", "+0+", "+0);
       ledArray.push(led);
    }
 
@@ -66,11 +67,16 @@ Morphasynth.Button = function(){
     wi = butBox.width;
     he = butBox.height;
 
+    //put the name
+/*    nameText = synthCanvas.text(x, y, name);
+    nameText.attr({'font-family':'synth','font-size':'18','fill':ledon});*/
+
+    this.setAngle(-100);
   }
 
-  this.getTouch = function (xt, yt){
+  this.getTouch = function (xt, yt, off){
 
-    if(xt>x && xt<(x+wi) && yt>y && yt<(y+he)){
+    if(xt>(x-off) && xt<(x+wi+off) && yt>(y-off) && yt<(y+he+off)){
       isTouched = true;
     }else{
       isTouched = false;
@@ -107,7 +113,7 @@ Morphasynth.Button = function(){
     //setLeds
     this.turnLeds();
 
-    console.log("value of "+name+": "+angle);
+    console.log("value of "+name+": "+ang);
   };
 
   this.turnLeds = function (){
@@ -120,11 +126,14 @@ Morphasynth.Button = function(){
     for(var i=0 ; i<this.map(angle,0,100,0,15) ; i++){
       ledArray[i].attr({'fill':ledon});
     }
-
   };
 
   this.getName = function (){
     return name;
+  };
+
+  this.setName = function (str){
+    name = str;
   };
 
   this.map = function (value, start1, stop1, start2, stop2) {
--- html/js/clasic-synth.js
+++ html/js/clasic-synth.js
@@ -7,20 +7,24 @@ Morphasynth.ClasicSynth = function(){
   var cs;
   var currentButton = null;
   var buttons = new Array();
+  var butNames;
 
-  this.init = function (){
+  this.init = function (ky){
 
     var self = this;
+
+    butNames = ["osc1-amplitude","osc1-waveform","osc1-finetune","osc2-amplitude","osc2-waveform","osc2-finetune","osc3-amplitude","osc3-waveform","osc3-finetune","lp-freq","lp-resonance","lp-envelope-a","lp-envelope-d","hp-freq","hp-resonance","hp-envelope-a","hp-envelope-d","lp-keyfollow","lp-contour","lp-envelope-s","lp-envelope-r","hp-keyfollow","hp-contour","hp-envelope-s","hp-envelope-r","lp-lfo-freq","lp-lfo-waveform","lp-lfo-amplitude","noise-amplitude","hp-lfo-freq","hp-lfo-waveform","hp-lfo-amplitude","lfo1-freq","lfo1-waveform","lfo2-freq","lfo2-waveform","lfo3-freq","lfo3-waveform","lfo1-to-amplitude","lfo1-to-freq","lfo2-to-amplitude","lfo2-to-freq","lfo3-to-amplitude","lfo3-to-freq","envelope-a","envelope-d","envelope-s","envelope-r"];
+    console.log("nams: "+butNames.length);
 
     cs = $("#clasic-synth");
     this.synthCanvas = Raphael('clasic-synth', cs.width(), cs.height());
 
     //create buttons
-    for(var i=0 ; i<5 ; i++){
+    for(var i=0 ; i<48 ; i++){
       var button = new Morphasynth.Button();
-      button.init("papucho "+i, this.synthCanvas);
+      button.init(""+butNames[i], this.synthCanvas);
       buttons.push(button);
-    }
+    };
     console.log("butones: "+buttons.length);
 
     //stop browser scrolling
@@ -29,7 +33,9 @@ Morphasynth.ClasicSynth = function(){
     //set Background
     var bg = this.synthCanvas.rect(0,0,cs.width(),cs.height());
     bg.attr("fill", "#002129");
-    self.BackgroundBeauties(cs.width(), cs.height());
+    
+    //put background dots
+    //self.BackgroundBeauties(cs.width(), cs.height());
 
     self.drawOn(this.synthCanvas);
 
@@ -56,8 +62,92 @@ Morphasynth.ClasicSynth = function(){
     title.attr("font-size","25");
 
     //put buttons on canvas
-    for (var i = buttons.length - 1; i >= 0; i--) {
-      buttons[i].draw(70+(i*100), 115);
+
+    //first row
+    var cnt = 0;
+    for (var i = 0; i <9; i++) {
+      buttons[i].draw(48+(cnt), 118);
+      ttt = this.synthCanvas.text(48+(cnt), 80, butNames[i]);
+      ttt.attr({'font-family':'synth','font-size':'18','fill':"#B9E9E1"});
+      cnt += 82;
+      if((i+1)%3 == 0){
+        cnt+=10;
+      }
+    };
+
+    //second row
+    cnt = 0;
+    for (var i = 9; i < 17; i++) {
+      buttons[i].draw(50+(cnt), 221);
+      ttt = this.synthCanvas.text(50+(cnt), 129, butNames[i]);
+      ttt.attr({'font-family':'synth','font-size':'18','fill':"#B9E9E1"});
+      cnt += 83;
+      if((i)%2 == 0){
+        cnt+=18;
+        if(i%4==0){
+          cnt+=25;
+        }
+      }
+    };
+
+    //third row
+    cnt = 0;
+    for (var i = 17; i < 25; i++) {
+      buttons[i].draw(50+(cnt), 308);
+      ttt = this.synthCanvas.text(50+(cnt), 174, butNames[i]);
+      ttt.attr({'font-family':'synth','font-size':'18','fill':"#B9E9E1"});
+      cnt += 83;
+      if((i)%2 == 0){
+        cnt+=18;
+        if(i%4==0){
+          cnt+=23;
+        }
+      }
+    };
+
+    //4th row 221
+    cnt = 0;
+    for (var i = 25; i < 32; i++) {
+      buttons[i].draw(78+(cnt), 420);
+      ttt = this.synthCanvas.text(78+(cnt), 230, butNames[i]);
+      ttt.attr({'font-family':'synth','font-size':'18','fill':"#B9E9E1"});
+      cnt += 93;
+      if(i == 27 || i == 28){
+        cnt+=27;
+      }
+    };
+
+    //4th row
+    cnt = 0;
+    for (var i = 32; i < 38; i++) {
+      buttons[i].draw(143+(cnt), 535);
+      ttt = this.synthCanvas.text(143+(cnt), 286, butNames[i]);
+      ttt.attr({'font-family':'synth','font-size':'18','fill':"#B9E9E1"});
+      cnt += 80;
+      if((i+1)%2 == 0){
+        cnt+=40;
+      }
+    };
+
+    //5th row
+    cnt = 0;
+    for (var i = 38; i < 44; i++) {
+      buttons[i].draw(143+(cnt), 621);
+      ttt = this.synthCanvas.text(143+(cnt), 328, butNames[i]);
+      ttt.attr({'font-family':'synth','font-size':'18','fill':"#B9E9E1"});
+      cnt += 80;
+      if((i+1)%2 == 0){
+        cnt+=40;
+      }
+    };
+
+    //6th row
+    cnt = 0;
+    for (var i = 44; i < 48; i++) {
+      buttons[i].draw(267+(cnt), 718);
+      ttt = this.synthCanvas.text(267+(cnt), 377, butNames[i]);
+      ttt.attr({'font-family':'synth','font-size':'18','fill':"#B9E9E1"});
+      cnt += 78;
     };
     
     //when touch starts
@@ -67,7 +157,7 @@ Morphasynth.ClasicSynth = function(){
         if(event.gesture.touches.length == 1){
 
           for (var i = buttons.length - 1; i >= 0; i--) {
-            if(buttons[i].getTouch(event.gesture.touches[0].pageX,event.gesture.touches[0].pageY)){
+            if(buttons[i].getTouch(event.gesture.touches[0].pageX,event.gesture.touches[0].pageY,20)){
               currentButton = buttons[i];
               //alert("yaerahh !!  "+currentButton.getName());
             }
